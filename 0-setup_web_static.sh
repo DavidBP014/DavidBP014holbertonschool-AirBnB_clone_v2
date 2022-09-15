fa8baccf76462bcc8139f85194bb4b34fdc4bb64
Juan David Ballesteros Perez <91141739+DavidBP014@users.noreply.github.com>
2022-09-14 21:43:59 -0500

    wlkecnlockr

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,13 @@
 #!/usr/bin/env bash
-# Bash script to setup a web server for the deployement of web_static.
+# sets up the web servers for the deployment of web_static
 
-sudo apt-get update -y
-sudo apt-get install -y nginx
-sudo service nginx start
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/ 
-sudo echo "This is my sample page" > /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test /data/web_static/current 
-sudo chown ubuntu:ubuntu -R /data/
-regex="^\tlocation+"
-location="\n\n\tlocation \/hbnb_static\/ \{\
-\n\t\talias \/data\/web_static\/current\/\;\
-\n\t\tautoindex off\;\
-\n\t\}"
-sudo sed -i -r "s/$regex/$location\n\n\0/g" /etc/nginx/sites-enabled/default
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared/
+sudo echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
